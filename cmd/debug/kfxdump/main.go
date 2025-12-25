2f83a32e449bf3bb60651bc0ab4ca98a3fa2a38a
rupor-github <rupor0mail@gmail.com>
2025-12-25 10:25:38 -0500

    Starting over

--- cmd/debug/kfxdump/main.go
+++ cmd/debug/kfxdump/main.go
@@ -3,11 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-
-	"github.com/amazon-ion/ion-go/ion"
-
-	"fbc/convert/kfx"
-	"fbc/convert/kfx/container"
 )
 
 func main() {
@@ -23,22 +18,5 @@ func main() {
 		os.Exit(1)
 	}
 
-	u, err := container.Unpack(b)
-	if err != nil {
-		_, _ = fmt.Fprintf(os.Stderr, "unpack %s: %v\n", path, err)
-		os.Exit(1)
-	}
-
-	fmt.Print(kfx.BuildDebugTree(u.ContainerID, len(u.DocumentSymbols), u.Fragments))
-
-	if u.ContainerInfo != nil {
-		if t, err := ion.MarshalText(u.ContainerInfo); err == nil {
-			fmt.Printf("\ncontainer_info=%s\n", string(t))
-		}
-	}
-	if u.FormatCapabilities != nil {
-		if t, err := ion.MarshalText(u.FormatCapabilities); err == nil {
-			fmt.Printf("\nformat_capabilities=%s\n", string(t))
-		}
-	}
+	_ = b
 }
